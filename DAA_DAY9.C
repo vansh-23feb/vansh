9d789aea2a2f6d57347c114ca9d63d263b4953b2
vansh-23feb <vanshjaiswal23022007@gmail.com>
2026-08-29 11:58:54 +0530

    Add files via upload

---
+++ DAA_DAY9.C
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5, 6, 7};
+    int n = 7;
+    int k = 3;
+
+    k = k % n;
+
+    printf("Rotated array: ");
+
+    for (int i = n - k; i < n; i++)
+        printf("%d ", arr[i]);
+
+    for (int i = 0; i < n - k; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
